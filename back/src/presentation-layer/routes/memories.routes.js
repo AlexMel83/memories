34276caacd9d01b5528a493b187b6335375b9ea0
AlexMel83 <meleshko.a.n@ukr.net>
2024-08-25 01:04:27 +0300

    +

---
+++ back/src/presentation-layer/routes/memories.routes.js
@@ -0,0 +1,32 @@
+import validateMiddleware from '../../middlewares/validate-middleware.js';
+// import authMiddleware from '../../middlewares/auth-middleware.js';
+import memoriesController from '../controllers/memories-controller.js';
+import { query } from 'express-validator';
+
+const validateQueryMemory = [
+  query('id')
+    .optional({ checkFalsy: true })
+    .isNumeric()
+    .withMessage('id is number'),
+  query('user_id')
+    .optional({ checkFalsy: true })
+    .isNumeric()
+    .withMessage('user_id is number'),
+  query('title')
+    .optional({ checkFalsy: true })
+    .isString()
+    .withMessage('title is string'),
+  query('description')
+    .optional({ checkFalsy: true })
+    .isString()
+    .withMessage('description is string'),
+];
+
+export default function (app) {
+  app.get(
+    '/memories',
+    validateQueryMemory,
+    validateMiddleware,
+    memoriesController.getMemories,
+  );
+}
