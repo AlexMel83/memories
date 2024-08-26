bd86e665a6ef1cedda073b9f30cda36ae4acd499
AlexMel83 <meleshko.a.n@ukr.net>
2024-08-26 21:31:25 +0300

    +

--- front/middleware/utils.js
+++
@@ -1,9 +0,0 @@
-export function debounce(fn, delay) {
-  let timeoutID = null;
-  return function (...args) {
-    clearTimeout(timeoutID);
-    timeoutID = setTimeout(() => {
-      fn.apply(this, args);
-    }, delay);
-  };
-}
