01167b2e33f2e8b8c9bb95e5c7e47be6c49381e4
AlexMel83 <meleshko.a.n@ukr.net>
2024-08-29 22:04:00 +0300

    15248

--- front/components/MapFeatures.vue
+++ front/components/MapFeatures.vue
@@ -8,6 +8,8 @@
         class="pl-9 pr-4 py-3 text-sm focus:outline-none w-full shadow-md rounded-md"
         type="text"
         placeholder="Пошук"
+        v-model="searchQuery"
+        @input="search"
       />
       <!--Search icon-->
       <UIcon
@@ -17,12 +19,21 @@
       <!--Search result-->
       <div class="absolute mt-2 w-full">
         <!--Result-->
-        <div class="h-[200px] overflow-scroll bg-white rounded-md">
-          <div
-            class="px-4 py-2 flex items-center gap=x-2 cursor-pointer hover:bg-slate-600 hover:text-white"
-          >
-            <UIcon name="i-heroicons-map-pin-solid" class="w-6 h-6" />
-            <p class="text-xs">Testing Result</p>
+        <div
+          v-if="searchQuery"
+          class="max-h-[200px] overflow-y-auto overflow-x-hidden bg-white rounded-md"
+        >
+          <!--Loading-->
+          <LoadingSpinner v-if="!searchData" />
+          <div v-else>
+            <div
+              v-for="(result, index) in searchData"
+              :key="index"
+              class="px-4 py-2 flex items-center gap-2 cursor-pointer hover:bg-slate-600 hover:text-white"
+            >
+              <UIcon name="i-heroicons-map-pin-solid" class="w-6 h-6" />
+              <p class="text-xs">{{ result.place_name_uk }}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -43,7 +54,38 @@
 </template>
 
 <script>
+import axios from 'axios';
+import { ref } from 'vue';
+import LoadingSpinner from './LoadingSpinner.vue';
 export default {
   props: ['coords', 'fetchCoords'],
+  components: { LoadingSpinner },
+  setup(props) {
+    const searchQuery = ref(null);
+    const searchData = ref(null);
+    const querTimeout = ref(null);
+    const search = () => {
+      clearTimeout(querTimeout.value);
+      searchData.value = null;
+      querTimeout.value = setTimeout(async () => {
+        if (searchQuery.value !== '') {
+          const params = new URLSearchParams({
+            fuzzyMatch: true,
+            language: 'uk',
+            limit: 10,
+            proximity: props.coords
+              ? `${props.coords.lng},${props.coords.lat}`
+              : '0,0',
+          });
+          const getData = await axios.get(
+            `http://localhost:4041/geosearch/${searchQuery.value}?${params}`,
+          );
+          searchData.value = getData.data.features;
+          console.log(searchData.value);
+        }
+      }, 750);
+    };
+    return { searchQuery, searchData, querTimeout, search };
+  },
 };
 </script>
