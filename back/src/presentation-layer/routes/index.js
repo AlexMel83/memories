2da4f121c86f172998513458209b67353e906eb9
AlexMel83 <meleshko.a.n@ukr.net>
2024-09-20 14:01:59 +0300

    +

--- back/src/presentation-layer/routes/index.js
+++ back/src/presentation-layer/routes/index.js
@@ -1,6 +1,7 @@
 import uploadsRouteInit from './uploads.routes.js';
 import authRouteInit from './auth.routes.js';
 import memoriesRouteInit from './memories.routes.js';
+import panoramasRouteInit from './panoramas.routes.js';
 import geoQueriesRouteInit from './geo-queries.routes.js';
 
 const routeInit = (app, express) => {
@@ -8,6 +9,7 @@ const routeInit = (app, express) => {
   app.use(express.json());
   authRouteInit(app);
   memoriesRouteInit(app);
+  panoramasRouteInit(app);
   uploadsRouteInit(app);
   geoQueriesRouteInit(app);
 };
