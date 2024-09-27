d4b453ef1bef31df7817083c5a94bd1a5cdca47e
AlexMel83 <meleshko.a.n@ukr.net>
2024-09-27 10:55:26 +0300

    +

--- front/pages/panoramas/[id].vue
+++ front/pages/panoramas/[id].vue
@@ -1,8 +1,20 @@
 <template>
   <div class="panorama-page">
     <div v-if="panorama" class="panorama-container">
-      <h1>{{ panorama.title }}</h1>
+      <h1 class="text-3xl font-bold text-gray-900 text-center m-4">
+        {{ panorama.title }}
+      </h1>
       <div ref="streetViewContainer" class="street-view" />
+      <div class="pagination">
+        <UButton
+          @click="previousPanorama"
+          :disabled="currentId <= 1"
+          class="mx-4"
+        >
+          Назад
+        </UButton>
+        <UButton @click="nextPanorama" class="mx-4">Вперед</UButton>
+      </div>
     </div>
     <div v-else class="loading">Загрузка панорамы...</div>
   </div>
@@ -13,25 +25,26 @@ import { ref, onMounted, nextTick } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 const panorama = ref(null);
 const streetViewContainer = ref(null);
+const currentId = ref(parseInt(route.params.id));
 const { $api } = useNuxtApp();
 
 onMounted(async () => {
+  await loadPanorama();
+});
+
+const loadPanorama = async () => {
   try {
-    // Получаем данные панорамы
-    const response = await $api.panoramas.getPanoramaById(route.params.id);
+    const response = await $api.panoramas.getPanoramaById(currentId.value);
     panorama.value = response.data[0];
-
-    // Дожидаемся следующего такта, чтобы DOM был готов
     await nextTick();
-
-    // Инициализация панорамы
     initStreetView();
   } catch (error) {
     console.error('Ошибка загрузки панорамы:', error);
   }
-});
+};
 
 const initStreetView = () => {
   if (panorama.value && streetViewContainer.value) {
@@ -39,21 +52,48 @@ const initStreetView = () => {
       lat: parseFloat(panorama.value.latitude),
       lng: parseFloat(panorama.value.longitude),
     };
-
-    // Инициализация Google Street View
-    new google.maps.StreetViewPanorama(streetViewContainer.value, {
-      position: location,
-      pov: {
-        heading: parseFloat(panorama.value.heading) || 0,
-        pitch: parseFloat(panorama.value.tilt) - 90 || 0,
+    const streetView = new google.maps.StreetViewPanorama(
+      streetViewContainer.value,
+      {
+        position: location,
+        pov: {
+          heading: parseFloat(panorama.value.heading) || 0,
+          pitch: parseFloat(panorama.value.tilt) - 90 || 0,
+        },
+        zoom: 0,
       },
-      zoom: 1,
+    );
+    streetView.addListener('zoom_changed', () => {
+      const currentZoom = streetView.getZoom();
+      console.log('Current zoom:', currentZoom);
+      if (document.fullscreenElement && currentZoom !== 0) {
+        streetView.setZoom(0);
+      }
+    });
+    document.addEventListener('fullscreenchange', () => {
+      if (document.fullscreenElement) {
+        streetView.setZoom(0);
+      }
     });
   } else {
     console.warn(
       'Контейнер для панорамы не готов или данные панорамы отсутствуют.',
     );
   }
+};
+
+const previousPanorama = () => {
+  if (currentId.value > 1) {
+    currentId.value -= 1;
+    router.push({ name: 'panoramas-id', params: { id: currentId.value } });
+    loadPanorama();
+  }
+};
+
+const nextPanorama = () => {
+  currentId.value += 1;
+  router.push({ name: 'panoramas-id', params: { id: currentId.value } });
+  loadPanorama();
 };
 </script>
 
@@ -71,5 +111,22 @@ const initStreetView = () => {
 .loading {
   text-align: center;
   font-size: 1.5em;
+}
+
+.pagination {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+button:disabled {
+  cursor: not-allowed;
+  opacity: 0.5;
 }
 </style>
--- front/pages/privacy.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="privacy bg-white h-screen" />
-</template>
